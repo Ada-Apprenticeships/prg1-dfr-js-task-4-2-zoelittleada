57dc448420b96135587db69c2199c783870cd400
zoelittleada <zoe.little@ada.ac.uk>
2024-10-29 10:00:41 +0000

    first 2 functions working

--- dfr.js
+++ dfr.js
@@ -5,7 +5,16 @@ function fileExists(filename) {
 }
 
 function validNumber(value) {
-  
+  if (typeof value === 'number'){
+    return !isNaN(value);
+  } else if (typeof value === 'string'){
+    const numberRule = /^-?\d+(\.\d+)?$/;
+    return numberRule.test(value);
+  } else {
+    return false;
+  }
+  //const numValue = Number(value)
+  //return !isNaN(numValue) && (typeof value === 'number'|| typeof value === 'string');
 }
 
 function dataDimensions(dataframe) {
