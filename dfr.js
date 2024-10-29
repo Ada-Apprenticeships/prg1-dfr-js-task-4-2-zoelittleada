460cacce28a1e62a7a7f22803f301bc65569c832
zoelittleada <zoe.little@ada.ac.uk>
2024-10-29 15:04:56 +0000

    ternary operators

--- dfr.js
+++ dfr.js
@@ -16,21 +16,15 @@ function validNumber(value) {
 }
 
 function dataDimensions(dataframe) {
-  if (!Array.isArray(dataframe)) {
-    return [-1, -1]; 
-  }
-  const numRows = dataframe.length;
-  if (numRows > 0 && Array.isArray(dataframe[0])){
-    return [numRows, dataframe[0].length];
-  } else {
-    return [numRows, -1];
-  }
+  return Array.isArray(dataframe) 
+  ? (dataframe.length > 0 && Array.isArray(dataframe[0]) 
+    ? [dataframe.length, dataframe[0].length] 
+    : [dataframe.length, -1]) 
+    : [-1, -1];
 }
 
 function findTotal(dataset) {
-  if (!Array.isArray(dataset)) {
-    return 0; 
-  }
+  if (!Array.isArray(dataset)) return 0; 
   let total = 0;
   for (let i = 0; i < dataset.length; i++) { 
     if (validNumber(dataset[i])){
@@ -41,33 +35,21 @@ function findTotal(dataset) {
 }
 
 function calculateMean(dataset) {
-  if (!Array.isArray(dataset) || dataset.length === 0){
-    return 0;
-  }
+  if (!Array.isArray(dataset) || dataset.length === 0) return 0;
   let total = findTotal(dataset);
   let length = dataset.filter(validNumber).length;
-  if (total === 0){
-    return 0;
-  } 
+  if (total === 0) return 0;
   return total/length; 
 }
 
 function calculateMedian(dataset) {
-  if (!Array.isArray(dataset) || dataset.length === 0) {
-    return 0; 
-  }
+  if (!Array.isArray(dataset) || dataset.length === 0) return 0;
   const orderNumbers = dataset.filter(validNumber).map(Number).sort((a,b)=> a-b);
-  const length = orderNumbers.length
-  if (length === 0){
-    return 0
-  } else if (length % 2 === 0){
-    const middle1 = length / 2 -1
-    const middle2 = length / 2
-    return (orderNumbers[middle1] + orderNumbers[middle2])/2
-  } else {
-    const middle = Math.floor(length/2)
-    return orderNumbers[middle] 
-  }
+  const length = orderNumbers.length;
+  if (length === 0) return 0;
+  return length % 2 === 0 
+    ? (orderNumbers[length / 2 - 1] + orderNumbers[length / 2]) / 2  
+    : orderNumbers[Math.floor(length / 2)];
 }
 
 function convertToNumber(dataframe, col) {
@@ -96,7 +78,7 @@ function loadCSV(csvFile, ignoreRows, ignoreCols) {
   const processedData = lines
     .filter((_, i) => !ignoreRows.includes(i)) 
     .map(line => line.split(',')
-      .filter((_, j) => !ignoreCols.includes(j)) 
+    .filter((_, j) => !ignoreCols.includes(j)) 
     );
   return [processedData, originalRows, originalCols]
 }
